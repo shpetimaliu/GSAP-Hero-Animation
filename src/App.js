e559ce8d5f66864e8eb48f3c5b44902259ef0eb0
Shpetim Aliu <106196770+shpetimaliu@users.noreply.github.com>
2023-05-22 16:57:57 +0200

    Update App.js
    I made some changes:
    
    In References, variables (appRef, imagesRef, contentRef) were directly used and accessed using .current.
    I stored the animations in a ref (tl) created with useRef and then used it to create the animations.
    And the animations for the headlineFirst, headlineSecond, and headlineThird elements are grouped in one line, taking advantage of the fact that these child elements follow each other.
    
    - These changes make the code more structured and general, without affecting its functionality.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,52 @@
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import "./App.scss";
-import {TimelineLite ,TweenMax, Power3} from 'gsap';
+import { TimelineLite, Power3 } from "gsap";
 
 import imgGirl from "./images/girl.webp";
 import imgBoy from "./images/boy.webp";
-import arrow from './images/arrow-right.svg'
+import arrow from "./images/arrow-right.svg";
 
 function App() {
-  let app = useRef(null)
-  let images = useRef(null)
-  let content = useRef(null)
-  let tl = new TimelineLite({ delay: .8});
-  
+  const appRef = useRef(null);
+  const imagesRef = useRef(null);
+  const contentRef = useRef(null);
+  const tl = useRef(new TimelineLite({ delay: 0.8 })).current;
+
   useEffect(() => {
-    // Images Vars
-    const girlImage = images.firstElementChild; // or children[0]
-    const boyImage = images.lastElementChild;
-    
-    //content vars
-    const headlineFirst = content.children[0].children[0];
-    const headlineSecond = headlineFirst.nextSibling;
-    const headlineThird = headlineSecond.nextSibling;
-    const contentP = content.children[1];
-    const contentButton = content.children[2];
+    const girlImage = imagesRef.current.firstElementChild;
+    const boyImage = imagesRef.current.lastElementChild;
+    const [headlineFirst, headlineSecond, headlineThird] = contentRef.current.children[0].children;
+    const contentP = contentRef.current.children[1];
+    const contentButton = contentRef.current.children[2];
 
-    //Remove initial flash
-    TweenMax.to(app, 0, {css: {visibility: 'visible'}})
+    tl.from(girlImage, 1.2, { y: 1280, ease: Power3.easeOut }, "Start")
+      .from(girlImage.firstElementChild, 2, { scale: 1.6, ease: Power3.easeOut }, 0.2)
+      .from(boyImage, 1.4, { y: 1280, ease: Power3.easeOut }, 0.2)
+      .from(boyImage.firstElementChild, 2, { scale: 1.6, ease: Power3.easeOut }, 0.2)
+      .staggerFrom([headlineFirst.children, headlineSecond.children, headlineThird.children], 1, {
+        y: 44,
+        ease: Power3.easeOut,
+        delay: 0.8,
+      }, 0.15, "Start")
+      .from(contentP, 1, { y: 20, opacity: 0, ease: Power3.easeOut }, 1.4)
+      .from(contentButton, 1, { y: 20, opacity: 0, ease: Power3.easeOut }, 1.6);
 
-    //Images Animation
-    tl.from(girlImage, 1.2, {y: 1280, ease: Power3.easeOut},'Start')
-    .from(girlImage.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .2)
-    .from(boyImage, 1.4, {y: 1280, ease: Power3.easeOut}, .2)
-    .from(boyImage.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .2)
-
-    //Content Animation
-    tl.staggerFrom([headlineFirst.children, headlineSecond.children, headlineThird.children ], 1, {
-      y: 44,
-      ease:Power3.easeOut,
-      delay: .8
-    }, .15, 'Start')
-    .from(contentP, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.4)
-    .from(contentButton, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.6)
-
-  }, [tl])
-
-
+    // Remove initial flash
+    tl.set(appRef.current, { css: { visibility: "visible" } });
+  }, [tl]);
 
   return (
-    <div className="hero" ref={el => app = el}>
+    <div className="hero" ref={appRef}>
       <div className="container">
         <div className="hero-inner">
           <div className="hero-content">
-            <div className="hero-content-inner" ref={el => content = el}>
+            <div className="hero-content-inner" ref={contentRef}>
               <h1>
                 <div className="hero-content-line">
                   <div className="hero-content-line-inner">Relieving the burden</div>
                 </div>
                 <div className="hero-content-line">
-                  <div className="hero-content-line-inner">of disease caused</div>  
+                  <div className="hero-content-line-inner">of disease caused</div>
                 </div>
                 <div className="hero-content-line">
                   <div className="hero-content-line-inner">by behaviors.</div>
@@ -69,16 +58,17 @@ function App() {
                 digital therapeutics.
               </p>
               <div className="btn-row">
-                <button className="explore-button">Explore
+                <button className="explore-button">
+                  Explore
                   <div className="arrow-icon">
-                    <img src={arrow} alt="row"/>
+                    <img src={arrow} alt="row" />
                   </div>
                 </button>
               </div>
             </div>
           </div>
           <div className="hero-images">
-            <div ref={el => images = el}  className="hero-images-inner">
+            <div className="hero-images-inner" ref={imagesRef}>
               <div className="hero-image girl">
                 <img src={imgGirl} alt="girl" />
               </div>
